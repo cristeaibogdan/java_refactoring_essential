fd32b6fae702ac09d8f40d6bfa1646d120460ce3
Bogdan <cristea.ibogdan@gmail.com>
2026-06-20 12:13:47 +0300

    Rename p to sum. Remove comment.

--- src/main/java/com/codemanship/refactoring/comments/X1.java
+++ src/main/java/com/codemanship/refactoring/comments/X1.java
@@ -3,16 +3,14 @@ package com.codemanship.refactoring.comments;
 public class X1 {
 
     static int m(int lowerBound, int upperBound) {
-        int p = 0;
+        int sum = 0;
 
-        // Iterate from lower bound (q) to upper bound (upperBound)
         for (int i = lowerBound; i <= upperBound; i++) {
             // Add square of each number in the range
-            p += calculateSquare(i);
+            sum += calculateSquare(i);
         }
 
-        // Return accumulated sum
-        return p;
+        return sum;
     }
 
     static int calculateSquare(int k) {
