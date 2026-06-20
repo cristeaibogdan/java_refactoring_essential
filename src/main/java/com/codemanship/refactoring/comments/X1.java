88f95eff8e4321b3a2b537b15d1f4701d3ed63f4
Bogdan <cristea.ibogdan@gmail.com>
2026-06-20 12:16:08 +0300

    Rename metho to sumRangeSquares and remove comment

--- src/main/java/com/codemanship/refactoring/comments/X1.java
+++ src/main/java/com/codemanship/refactoring/comments/X1.java
@@ -2,11 +2,10 @@ package com.codemanship.refactoring.comments;
 
 public class X1 {
 
-    static int m(int lowerBound, int upperBound) {
+    static int sumRangeSquares(int lowerBound, int upperBound) {
         int sum = 0;
 
         for (int i = lowerBound; i <= upperBound; i++) {
-            // Add square of each number in the range
             sum += calculateSquare(i);
         }
 
